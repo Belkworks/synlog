402132634a42d8a5f0c488d633f7d8999c1d08eb
safazi <safazi@airmail.cc>
2022-12-20 13:00:16 -0600

    no build

--- out/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { DrawingLogger } from "./class/logger";
-export { Line } from "./class/line";
-export { TextBlock } from "./class/block";
-export { Colors, Text } from "./helper";
-export declare const Synlog: DrawingLogger;
--- out/types.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="@rbxts/types" />
-export declare type Token = {
-    text: string;
-    color: Color3;
-    font?: Font | undefined;
-};
